876b1216fb142adabb345bb6df5dac64a529db2e
Brian Ginsburg <gins@brianginsburg.com>
2021-06-29 14:42:36 -0700

    Use window postMessage for messsage passing
    The data was not accessible of off the custom event object, window post
    message works better here.
    Add UI showing waiting message then screenshot and meta after capture.

--- app/main.js
+++ app/main.js
@@ -1,9 +1,15 @@
+const messageP = document.getElementById('message')
 const urlA = document.getElementById('url')
 const timestampSpan = document.getElementById('timestamp')
 const screenshotImg = document.getElementById('screenshot')
 
 
-document.addEventListener('offload', event => {
-  const data = event.detail;
-  console.log('Content script sent: ', data);
+window.addEventListener('message', event => {
+  const data = event.data.detail;
+
+  messageP.textContent = 'Viewing '
+  urlA.href = data.url;
+  urlA.textContent = data.url;
+  timestampSpan.textContent = data.timestamp;
+  screenshotImg.src = data.imageUri;
 });
--- extension/proxy.js
+++ extension/proxy.js
@@ -2,10 +2,7 @@ browser.runtime.onMessage.addListener(
   function (request, sender, sendResponse) {
       sendResponse({ farewell: "goodbye" });
 
-      // console.log("In content script: ", request.greeting)
-      // document.dispatchEvent(new CustomEvent('offload', { detail: request.greeting}));
-
       console.log("In content script: ", request)
-      document.dispatchEvent(new CustomEvent('offload', { detail: request}));
+      window.postMessage({ detail: request}, 'https://stupid-enormous-square-hero.fission.app/');
   }
 );
