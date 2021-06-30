ce6419e98a8741247bc0733bbe5d03394f571ba5
Brian Ginsburg <gins@brianginsburg.com>
2021-06-30 10:26:02 -0700

    Add persmissions, auth, and display captured page
    Add webnative permissions and auth. We show the user a sign in button when
    not authenticated. On auth, we show them a message to indicate we await saved
    pages from the extension. When the extension sends a page to save, we show it.

--- app/main.js
+++ app/main.js
@@ -1,15 +1,84 @@
+const scenarioSpan = document.getElementById('scenario');
+const signInButton = document.getElementById('sign-in')
+
 const messageP = document.getElementById('message')
 const urlA = document.getElementById('url')
 const timestampSpan = document.getElementById('timestamp')
 const screenshotImg = document.getElementById('screenshot')
 
 
-window.addEventListener('message', event => {
-  const data = event.data.detail;
+const fissionInit = {
+  permissions: {
+    app: {
+      name: 'pachy',
+      creator: 'bgins'
+    },
+    fs: {
+      public: [{ directory: ["saved-pages"] }]
+    }
+  }
+};
 
-  messageP.textContent = 'Viewing '
+webnative.initialize(fissionInit).then(async state => {
+  console.log(state)
+
+  switch (state.scenario) {
+    case webnative.Scenario.AuthSucceeded:
+    case webnative.Scenario.Continuation:
+      scenarioSpan.textContent = 'Signed in.';
+      show('app')
+
+      const fs = state.fs;
+
+      window.addEventListener('message', event => {
+        console.log(event)
+        displayCapturedPage(event.data.detail);
+      });
+
+      break;
+
+    case webnative.Scenario.NotAuthorised:
+    case webnative.Scenario.AuthCancelled:
+      scenarioSpan.textContent = 'Not signed in.';
+      show('sign-in');
+      break;
+  }
+
+  signInButton.addEventListener('click', () => {
+    console.log('signing in');
+    webnative.redirectToLobby(state.permissions);
+  });
+
+}).catch(error => {
+  switch (error) {
+    case 'UNSUPPORTED_BROWSER':
+      scenarioSpan.textContent = 'Unsupported browser.';
+      break;
+
+    case 'INSECURE_CONTEXT':
+      scenarioSpan.textContent = 'Insecure context.';
+      break;
+  }
+});
+
+
+const hide = (...ids) => {
+  ids.forEach(id => {
+    document.getElementById(id).style.display = 'none';
+  });
+};
+
+const show = (...ids) => {
+  ids.forEach(id => {
+    const el = document.getElementById(id);
+    el.style.display = 'block';
+  });
+};
+
+const displayCapturedPage = (data) => {
+  screenshotImg.src = data.imageUri;
+  messageP.textContent = ''
   urlA.href = data.url;
   urlA.textContent = data.url;
   timestampSpan.textContent = data.timestamp;
-  screenshotImg.src = data.imageUri;
-});
+}
