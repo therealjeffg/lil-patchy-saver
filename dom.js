c89af19f56bcb12f90dfb1ba86d8c7e03894ecdf
Brian Ginsburg <gins@brianginsburg.com>
2021-06-30 13:12:54 -0700

    Add localization to displayed dates

--- dom.js
+++ dom.js
@@ -21,7 +21,23 @@ const displayCapturedPage = (data) => {
   messageP.textContent = ''
   urlA.href = data.url;
   urlA.textContent = data.url;
-  timestampSpan.textContent = data.timestamp;
+  timestampSpan.textContent = formatDate(data.timestamp);
+}
+
+function formatDate(timestamp) {
+  const date = new Date(timestamp);
+  const lang = navigator.language;
+  const formatOptions = {
+    dateStyle: 'long',
+    timeStyle: 'short'
+  };
+
+  const formattedDate = new Intl.DateTimeFormat(
+    lang,
+    formatOptions
+  ).format(date);
+
+  return formattedDate;
 }
 
 export { displayCapturedPage, hide, show };
